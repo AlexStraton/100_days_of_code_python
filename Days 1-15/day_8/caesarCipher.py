3c6e256de4c0909588d934fbb289ca034a86de6e
Alex Straton <alexandrastraton1@gmail.com>
2024-10-13 12:42:17 +0100

    day 8 caesar cipher: created basic function which shifts the words to the specific amount

---
+++ Days 1-15/day_8/caesarCipher.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(text, shift):
+    encrypted = ''
+    for letter in text:
+        if letter in alphabet:
+            alphabet_index = alphabet.index(letter)
+            shifted_index = alphabet_index + shift
+            encrypted += alphabet[shifted_index]
+    print(encrypted)
+    return encrypted
+encrypt(text, shift)
