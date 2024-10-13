b8a96fb18ec9b36fe15d10898c5cb54d25afc371
Alex Straton <alexandrastraton1@gmail.com>
2024-10-13 14:48:44 +0100

    added a while loop to allow user to go again

--- Days 1-15/day_8/caesarCipher.py
+++ Days 1-15/day_8/caesarCipher.py
@@ -1,17 +1,42 @@
+# TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+
+print(logo)
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+# TODO-2: What happens if the user enters a number/symbol/space?
 
-def caesar(direction, text, shift):
-    cipher_text = ""
-    for letter in text:
-            if direction == 'decode':
-                shift *= -1
-            shifted_position = alphabet.index(letter) + shift
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
             shifted_position %= len(alphabet)
-            cipher_text += alphabet[shifted_position]
-    print(f"Here is the {direction}d result: {cipher_text}")
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-caesar(direction, text, shift)
+
+
+# TODO-3: Can you figure out a way to restart the cipher program?
+go_again = True
+
+while go_again:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(text, shift, direction)
+    restart = input("Do you want to restart the cipher program? Type 'yes' or 'no':\n").lower()
+
+    if restart == 'no':
+        go_again = False
+
+
+
